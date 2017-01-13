7ea311fcbe3008ce9efbd41f2fdb9e198fd101b8
adventure-yunfei <adventure030@gmail.com>
2017-01-13 15:19:57 +0800

    使用自己的Promise库

--- meteor-killers-game/imports/setupPromise.js
+++ meteor-killers-game/imports/setupPromise.js
@@ -1,38 +1,22 @@
-// Promise
-Promise.prototype.finally = (function () {
-    return function promiseFinally(handler) {
-        const execHandler = () => {
-            try {
-                handler();
-            } catch (e) {
-                setTimeout(() => {
-                    throw e;
-                }, 0);
-            }
-        };
-        this.then((value) => {
-            execHandler();
-            return value;
-        }, (reason) => {
-            execHandler();
-            return Promise.reject(reason);
-        });
+import { Meteor } from 'meteor/meteor';
+import Promise from 'beauty-promise';
 
-        return Promise.resolve(this);
-    }
-})();
-
-window.addEventListener('unhandledrejection', event => {
-    const {reason} = event;
-    let errMsg = '';
-    if (typeof reason === 'string') {
-        errMsg = reason;
-    } else if (typeof reason.error === 'string') {
-        errMsg = reason.error 
-    } else if (typeof reason.reason === 'string') {
-        errMsg = reason.reason;
-    } else if (typeof reason.message === 'string') {
-        errMsg = reason.message;
-    }
-    alert(errMsg || reason.toString());
-});
+if (Meteor.isClient) {
+    window.Promise = Promise;
+    Promise.onUnhandledRejection = reason => {
+        let errMsg = '';
+        if (typeof reason === 'string') {
+            errMsg = reason;
+        } else if (reason && typeof reason.error === 'string') {
+            errMsg = reason.error 
+        } else if (reason && typeof reason.reason === 'string') {
+            errMsg = reason.reason;
+        } else if (reason && typeof reason.message === 'string') {
+            errMsg = reason.message;
+        }
+        alert(errMsg || (reason && reason.toString()) || 'Unknown error occured...');
+    };
+}
+if (Meteor.isServer) {
+    global.Promise = Promise;
+}

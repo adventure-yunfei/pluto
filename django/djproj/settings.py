0e28f71f35c420eaef98275e0cf7c82baac0494d
adventure-yunfei <adventure-yunfei@gmail.com>
2023-12-13 18:01:07 +0800

    add django password

--- django/djproj/settings.py
+++ django/djproj/settings.py
@@ -1,5 +1,10 @@
 # Django settings for djproj project.
 from os import path
+import json
+
+globalConfigJsonFile = open(path.abspath(path.join(path.dirname(__file__), '..', '..', 'config.json')), 'r')
+config = json.load(globalConfigJsonFile)
+globalConfigJsonFile.close()
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
@@ -21,7 +26,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'django',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'PASSWORD': config.django-photosite.mysql_password,                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
